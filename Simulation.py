28e71d0c963301a3980c0bd6da58d465dad8762d
rcgonzalez9061 <rgonzalez9061@gmail.com>
2020-01-02 15:09:21 -0800

    Initial commit

---
+++ Simulation.py
@@ -0,0 +1,13 @@
+import json
+
+class Simulation:
+    DEFAULT_POST_CHANCE = 0.1
+    DEFAULT_NUM_USERS = 100
+    DEFAULT_TICK_LIMIT = -1 # no limit default
+    
+    def __init__(self, parameters):
+        this.parse_parameters()
+        users = 
+    
+    def parse_parameters(self):
+        
